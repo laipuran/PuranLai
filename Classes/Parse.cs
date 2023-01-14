69f92ee371cf01c5e8b916a91a77355b120334a4
Puran Lai <1696095803@qq.com>
2023-01-14 15:14:22 +0800

    Splitted the previous methods and classes

---
+++ Classes/Parse.cs
@@ -0,0 +1,62 @@
+﻿namespace PuranLai.Algorithms
+{
+    /// <summary>
+    /// A structure of the Parse method
+    /// </summary>
+    internal struct ParsingResult
+    {
+        public int number;
+        public string message;
+
+        public ParsingResult()
+        {
+            number = 0;
+            message= string.Empty;
+        }
+    }
+
+    internal class Parse
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str">The string you want to parse int from.</param>
+        /// <param name="max">The range you want to limit the parsed int.</param>
+        /// <returns>A ParsingResult type struct</returns>
+        /// <exception cref="CustomException"></exception>
+        public static ParsingResult ParseFromString(string str, int max)
+        {
+            ParsingResult result = new();
+            int number;
+            try
+            {
+                number = int.Parse(str);
+                if (number <= 0)
+                {
+                    throw new CustomException("Illegal input string!");
+                }
+                if (number > max)
+                {
+                    throw new CustomException("The input number is not within range!");
+                }
+            }
+            catch (Exception Ex)
+            {
+                result.message = Ex.Message;
+                return result;
+            }
+            result.number = int.Parse(str);
+            return result;
+        }
+
+    }
+
+    internal class CustomException : Exception
+    {
+        /// <summary>
+        /// An exception you can customize the message.
+        /// </summary>
+        /// <param name="message">The message you want to show.</param>
+        public CustomException(string message) : base(message) { }
+    }
+}
---
+++ Classes/Random.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuranLai.Algorithms
+{
+    interface IRand
+    {
+        int[] GetInts();
+    }
+
+    internal class Rand : IRand
+    {
+        public static int Count;
+        public static int Max;
+
+        public Rand(int count, int max) {
+            Count = count;
+            Max = max;
+        }
+
+        /// <summary>
+        /// This method will get some random numbers within range and there is no reapeating
+        /// </summary>
+        /// <returns>An int array</returns>
+        public int[] GetInts()
+        {
+
+            int[] array = new int[Count];
+            int[] check = new int[Rand.Max];
+            Array.Clear(array, 0, Count);
+            Array.Clear(check, 0, Rand.Max);
+
+            Random random = new();
+            for (int i = 0; i < Count; i++)
+            {
+                int nextRandom = random.Next(1, Rand.Max + 1);
+                check[nextRandom - 1]++;
+                Thread.Sleep(5);
+            }
+            
+            bool checkValue = true;
+            while (checkValue == true)
+            {
+                for (int i = 0; i < Rand.Max; i++)
+                {
+                    if (check[i] > 1)
+                    {
+                        check[i]--;
+                        int temp = random.Next(1, Rand.Max + 1);
+                        check[temp - 1]++;
+                    }
+                }
+                int index = 0;
+                for (int i = 0; i < Max; i++)
+                {
+                    if (check[i] == 1 || check[i] == 0)
+                    {
+                        if (check[i] == 1)
+                        {
+                            array[index] = i + 1;
+                            index++;
+                        }
+                        if (i == Max - 1)
+                        {
+                            checkValue = false;
+                        }
+                    }
+                    else break;
+                }
+
+            }
+
+            for (int i = Count - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp;
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+
+}
